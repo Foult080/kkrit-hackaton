10b920f81577cbed3d7ace8cfa00b3351645b2e1
Foult080 <foult080@gmail.com>
2021-05-17 10:56:34 +0700

    added contact link

--- front-hack/src/Components/Layout/Footer.js
+++ front-hack/src/Components/Layout/Footer.js
@@ -6,6 +6,8 @@ import {
   List,
   Icon,
   Segment,
+  Popup,
+  Button,
 } from "semantic-ui-react";
 
 const Footer = () => {
@@ -18,8 +20,28 @@ const Footer = () => {
               <Header inverted as="h4" content="О проекте" />
               <List link inverted>
                 <List.Item as="a">О проекте</List.Item>
-                <List.Item as="a">Обратная связь</List.Item>
-                <List.Item as="a" href="/privacy">Политика конфиденциальности</List.Item>
+                <List.Item as="a">
+                  <Popup
+                    content={
+                      <>
+                        Если вы нашли ошибку или вам есть что предложить, то
+                        пожалуйста заполните форму на{" "}
+                        <a
+                          href="https://github.com/Foult080/kkrit-hackaton/issues"
+                          target="_blank"
+                        >
+                          Github.
+                        </a>
+                      </>
+                    }
+                    on="click"
+                    popper={{ id: "popper-container", style: { zIndex: 2000 } }}
+                    trigger={<p>Обратная связь</p>}
+                  />
+                </List.Item>
+                <List.Item as="a" href="/privacy">
+                  Политика конфиденциальности
+                </List.Item>
               </List>
             </Grid.Column>
             <Grid.Column width={4}>
@@ -29,8 +51,17 @@ const Footer = () => {
                 <Icon name="registered" />
               </h4>
               <List link inverted>
-                <List.Item as="a" href="https://github.com/Foult080" target="blanc">Creaated by @foult080</List.Item>
-                <List.Item as="a"href="https://github.com/Foult080/kkrit-hackaton">
+                <List.Item
+                  as="a"
+                  href="https://github.com/Foult080"
+                  target="blanc"
+                >
+                  Creaated by @foult080
+                </List.Item>
+                <List.Item
+                  as="a"
+                  href="https://github.com/Foult080/kkrit-hackaton"
+                >
                   <Icon name="github" />
                   Like project on Github
                 </List.Item>
